fb7aa03aaa70c7cd5920f3d436d0932c9d910cd1
Vyacheslav <myeve2007@yandex.ru>
2020-01-04 11:49:31 +0300

    feat: add webpack, eslint, editorconfig

---
+++ simple-piskel-clone/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "airbnb",
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "rules": {
+        "no-console": "off",
+        "max-len": "off",
+        "no-plusplus": "off",
+        "no-shadow": "off",
+        "no-undef": "off",
+        "no-new": "off",
+        "camelcase": "off",
+        "prefer-destructuring": "off"
+    }
+};
---
+++ simple-piskel-clone/webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const CssPlugin = require('mini-css-extract-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        polyfill: '@babel/polyfill',
+        main: './src/screens/canvas/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                enforce: 'pre',
+                test: /\.js$/,
+                exclude: '/node_modules/',
+                loader: 'eslint-loader'
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    presets: [
+                        '@babel/preset-env'
+                    ]
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: { minimize: false }
+                    }
+                ]
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: ['style-loader', CssPlugin.loader, 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlPlugin({
+            inject: false,
+            template: './src/screens/canvas/index.html',
+            filename: 'index.html'
+        }),
+
+        new CssPlugin({
+            filename: 'style.css'
+        })
+    ],
+    mode: 'development',
+    devtool: process.env.NODE_ENV === 'production'
+        ? 'hidden-source-map' : 'inline-source-map',
+    watchOptions: {
+        aggregateTimeout: 600,
+        poll: true
+    }
+};
